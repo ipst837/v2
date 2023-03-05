89826a395fe2762833b02089f77cda360a958e05
ipst837 <96713892+ipst837@users.noreply.github.com>
2023-03-05 14:07:01 +0900

    Update settings.py

--- GamingSite/GamingSite/settings.py
+++ GamingSite/GamingSite/settings.py
@@ -137,7 +137,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("lightsailredis-ro.kjifdv.ng.0001.apn2.cache.amazonaws.com:6379", 6379)],
+            "hosts": [("lightsailredis-ro.kjifdv.ng.0001.apn2.cache.amazonaws.com", 6379)],
         },
     },
 }
